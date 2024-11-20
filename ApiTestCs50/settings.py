f3485c1892c60c552b6aea303fb4a05ca0df1389
Jonathan <Jonathan106e@gmail.com>
2024-11-19 21:12:31 -0600

    Corregido el error de importación del módulo os

--- ApiTestCs50/settings.py
+++ ApiTestCs50/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,4 +129,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Don't forget to import os at the beginning of the file
-import os
